96b9cf9c08d02d1f7071e99dc769dfdb43c5a645
Andy Albaugh <andypsu5155@gmail.com>
2023-12-19 23:58:41 -0500

    Finished Skills. 4:29:47

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Intro from '@/components/intro'
 import SectionDivider from '@/components/section-divider'
 import Image from 'next/image'
 import Projects from '@/components/projects'
+import Skills from '@/components/skills'
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <SectionDivider />
       <About />
       <Projects />
+      <Skills />
     </main>
   )
 }
--- components/projects.tsx
+++ components/projects.tsx
@@ -11,7 +11,7 @@ export default function Projects() {
   const { ref } = useSectionInView('Projects', 0.65);
 
   return (
-    <section ref={ref} id="projects" className="scroll-mt-28">
+    <section ref={ref} id="projects" className="scroll-mt-28 mb-28">
         <SectionHeading>My Projects</SectionHeading>
         <div>
             {
